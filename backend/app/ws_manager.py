8545e9170309aefb3cd89c90e45730195a3c5a16
Sanjib <sanjibpoudel49@gmail.com>
2026-02-26 21:12:54 +0545

    feat: add connection manager

--- backend/app/ws_manager.py
+++ backend/app/ws_manager.py
@@ -1,21 +1,21 @@
-# from fastapi import WebSocket
-# from typing import Dict, List
+from fastapi import WebSocket
+from typing import Dict, List
 
-# class ConnectionManager:
-#     def __init__(self):
-#         self.active_connection: Dict[int, List[WebSocket]] = {} # type definition
+class ConnectionManager:
+    def __init__(self):
+        self.active_connection: Dict[int, List[WebSocket]] = {} # type definition
 
-#     async def connect(self, chat_id:int, websocket: WebSocket):
-#         await websocket.accept()
-#         if chat_id not in self.active_connection:
-#             self.active_connection[chat_id] = []
-#         self.active_connection[chat_id].append(websocket)
+    async def connect(self, chat_id:int, websocket: WebSocket):
+        await websocket.accept()
+        if chat_id not in self.active_connection:
+            self.active_connection[chat_id] = []
+        self.active_connection[chat_id].append(websocket)
 
-#     def disconnect(self, chat_id:int, websocket:WebSocket):
-#         self.active_connection[chat_id].remove(websocket)
-#         if not self.active_connection[chat_id]:
-#             del self.active_connection[chat_id]
+    def disconnect(self, chat_id:int, websocket:WebSocket):
+        self.active_connection[chat_id].remove(websocket)
+        if not self.active_connection[chat_id]:
+            del self.active_connection[chat_id]
 
-#     async def broadcast(self, chat_id:int, message:dict):
-#         for connection in self.active_connection.get(chat_id,[]):
-#             await connection.send_json(message)
+    async def broadcast(self, chat_id:int, message:dict):
+        for connection in self.active_connection.get(chat_id,[]):
+            await connection.send_json(message)
